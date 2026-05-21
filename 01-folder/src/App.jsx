fdc48721c5e35537e7cd81011855ba176322e59e
My-project <sauravkrss857881@gmail.com>
2026-05-21 09:25:26 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,25 @@
+// const App = () =>{
+//   return(
+//     <div id="parent">
+//          <h1 id="child1">Hello</h1>
+//          <h2 id="child2">Saurav</h2>
+//     </div>
+    
+//   )
+// }
+
+// export default App
+
+const App = () =>{
+  return(
+    <>
+      <div className="parent">
+        <div className="child1">Saurav</div>
+        <div className="Child2">Kumar</div>
+      </div>
+      <div className="chacha">ID</div>
+    </>
+  )
+}
+
+export default App;
